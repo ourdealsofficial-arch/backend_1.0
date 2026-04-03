cfa71137e76b93aef96728b0c576eaa2c2b26c58
ourdealsofficial-arch <ourdealsofficial@gmail.com>
2026-04-03 21:32:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,14 +12,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173" || "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors({
+  origin: "https://www.ncb.ourdeals.in",
+  credentials: true
+}));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/food", foodRouter);
